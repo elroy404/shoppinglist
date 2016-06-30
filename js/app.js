109366ace2c54691f137a17c26b4534deccb4589
elroy404 <Geralle@GMac.attlocal.net>
2016-06-29 17:42:54 -0700

    6/29 v2

--- js/app.js
+++ js/app.js
@@ -5,7 +5,26 @@ $(document).ready(function(){
     $('.fa-check').hide();
   });
 
+  $(".add-button").click(function(){
+    event.preventDefault();
+    var value = $(".txt-box").val();
+    console.log(value);
+  });
 
+//   $(".add-item").on("click","add-button",function(event){
+//     console.log("button click");
+//   var itemToAdd = $(this).parent().children(".add-item__input").val();
+//   if (itemToAdd != ""){
+//     console.log(itemToAdd);
+//     $(".stuff-i-need__list").prepend(
+//       "<li class=\"sl-item\"><span class=\"side check\">&#x2713;</span><span class=\"middle\">"
+//       +itemToAdd+"</span><span class=\"side ex\">&#x2717;</span></li>");
+//     $(this).parent().children(".add-item__input").val("");
+//   }
+//   else {
+//     alert("Please type in the box to add items");
+//   }
+// });
 
 
 
