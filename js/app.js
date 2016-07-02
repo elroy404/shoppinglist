6909694e8dcabe45f94129c8b4fd9767d6ae4580
elroy404 <Geralle@GMac.local>
2016-07-01 19:00:34 -0700

    7/1 v1

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     event.preventDefault();
     var listValue = $(".txt-box").val();
     identifier++;
-    var addeditem = "<li class=\"item-container\" data-id=\"" + identifier + "\"><span class=\"fa-stack fa-lg checkbox\">"+
+    var addeditem = "<li class=\"item-container\" data-id=\"" + identifier + "\"><span class=\"fa-stack fa-lg checkbox\" id=\"checkbox\" data-id=\"5\">" +
     "<i class=\"fa fa-square-o fa-stack-2x\"></i><i class=\"fa fa-check fa-stack-1x check\"></i></span>"+
     "<span class=\"item\">" + listValue + "</span></li>";
 
@@ -32,7 +32,7 @@ $(document).ready(function(){
   //   console.log(completedValue);
   // });
 
-  $(".items>.item-container").on("click",function(){
+  $("span#checkbox").on("click",function(){
     var completedValue = $(this).data("id");
     console.log(completedValue);
   });
