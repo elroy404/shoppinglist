75bea7913e38456520607122c488a970beeed225
elroy404 <Geralle@GMac.local>
2016-07-01 18:19:42 -0700

    7/1 v1

--- js/app.js
+++ js/app.js
@@ -7,16 +7,15 @@ $(document).ready(function(){
   //   $('.check').hide();
   // });
 
-  var identifier = 0;
+  var identifier = 1;
 
   $(".add-button").click(function(event){
     event.preventDefault();
     var listValue = $(".txt-box").val();
     identifier++;
-    var addeditem = "<li class=\"item-container\" id=\"container-" + identifier +
-    "\"><span class=\"fa-stack fa-lg checkbox\">"+
+    var addeditem = "<li class=\"item-container\" data-id=\"" + identifier + "\"><span class=\"fa-stack fa-lg checkbox\">"+
     "<i class=\"fa fa-square-o fa-stack-2x\"></i><i class=\"fa fa-check fa-stack-1x check\"></i></span>"+
-    "<span class=\"item\" id=\"item-\"" + identifier + ">"+ listValue + "</span></li>";
+    "<span class=\"item\">" + listValue + "</span></li>";
 
     if (listValue != ""){
       $(".items").append(addeditem);
@@ -28,8 +27,13 @@ $(document).ready(function(){
     }
   });
 
-  $(".items").click(function(){
-    var completedValue = $("span#item").text();
+  // $(".items").click(function(event){
+  //   var completedValue = $(this).text();
+  //   console.log(completedValue);
+  // });
+
+  $(".items>.item-container").on("click",function(){
+    var completedValue = $(this).data("id");
     console.log(completedValue);
   });
 });
